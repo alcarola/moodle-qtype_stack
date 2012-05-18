52fb5250c3320b28605691da1209fa48bd76aeb3
Mikael Kurula <alcarola@gmail.com>
2012-05-18 23:31:16 +0200

    Added save and continue button

--- lang/en/qtype_stack.php
+++ lang/en/qtype_stack.php
@@ -174,6 +174,9 @@ $string['variantsselectionseed'] = 'Random group';
 $string['variantsselectionseed_help'] = 'Normally you can leave this box blank. If, however, you want two different questions in a quiz to use the same random seed, then type the same string in this box for the two questions (and deploy the same set of random seeds, if you are using deployed versions) and the random seeds for the two questions will be synchronised.';
 $string['verifyquestionandupdate'] = 'Verify the question text and update the form';
 
+// MiK
+$string['saveandcontinue'] = 'Save the question and continue editing';
+
 // Error reporting in the feedback form.
 $string['edit_form_error'] = 'Node {$a->no} {$a->field} has the following error: ';
 
